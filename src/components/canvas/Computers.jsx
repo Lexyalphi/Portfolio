fcb36fafe9d286801b0db6106628cbbbfbcc665f
Lexyalphi <lexyalphi14@gmail.com>
2023-04-18 21:28:35 +0100

    Built the Experience section. Modified the Computer Canvas, Hero, About and started with the Tech section. Added a Hoc folder which contains the SectionWrapper.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,14 +4,15 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <pointLight intensity={1} />
       <spotLight
-        position={[-20, 50, 10]}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         angle={0.12}
         penumbra={1}
         intensity={1}
@@ -20,7 +21,7 @@ const Computers = () => {
       />
       <primitive 
         object={computer.scene}
-        scale={0.75}
+        scale={isMobile ? 0.7 : 0.75}
         position={[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -29,6 +30,21 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect (() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+  
+
   return(
     <Canvas
       frameloop="demand"
@@ -42,7 +58,7 @@ const ComputersCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
